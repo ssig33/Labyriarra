53392372f9153cd5e9826bd3c57f0b71ae7e618e
ssig33 <mail@ssig33.com>
2013-03-07 22:12:09 +0900

    ログインのルーティング作りわすれてた

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ WebTiarra::Application.routes.draw do
   match "/next" => "home#next", as: :next
   match "/log/:id/:date" => "home#log", as: :log
   match "/api/update_channels" => "api#update_channels"
+
+  get "/sessions" => 'sessions#index'
+  post '/sessions' => 'sessions#create'
   #match ':controller(/:action(/:id(.:format)))'
 end
